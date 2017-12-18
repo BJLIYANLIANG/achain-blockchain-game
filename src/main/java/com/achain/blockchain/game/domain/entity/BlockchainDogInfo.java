e0f7815138582b084de95e8e80317803f74a507d
yujianjian <yujianjian@new4g.cn>
2017-12-18 14:26:51 +0800

    refactor code

--- src/main/java/com/achain/blockchain/game/domain/entity/BlockchainDogInfo.java
+++ src/main/java/com/achain/blockchain/game/domain/entity/BlockchainDogInfo.java
@@ -6,18 +6,20 @@ import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author yujianjian
  * @since 2017-12-12 下午3:35
  */
+@EqualsAndHashCode(callSuper = true)
 @TableName("blockchain_dog_info")
 @Data
-public class BlockchainDogInfo implements Serializable{
+public class BlockchainDogInfo extends OrderBase implements Serializable {
 
     private static final long serialVersionUID = 2919726852864028990L;
 
-    private Integer id;
+
     private Integer dogId;
     private String nickname;
     private String owner;
@@ -27,9 +29,9 @@ public class BlockchainDogInfo implements Serializable{
     private Integer motherId;
     private Integer fatherId;
     private Integer generation;
-    /**是否可育,0-否,1-是*/
+    /**
+     * 是否可育,0-否,1-是
+     */
     private Integer fertility;
-    private Date createTime;
-    private Date updateTime;
-    private String trxId;
+
 }
