1d3ef39d8fbad6268bf28638d426a91872bb77bf
yujianjian <yujianjian@new4g.cn>
2017-12-19 08:53:52 +0800

    recover code

--- src/main/java/com/achain/blockchain/game/domain/entity/BlockchainRecord.java
+++ src/main/java/com/achain/blockchain/game/domain/entity/BlockchainRecord.java
@@ -6,25 +6,33 @@ import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 /**
  * @author yujianjian
  * @since 2017-12-11 下午1:56
  */
-@EqualsAndHashCode(callSuper = true)
+
+/**
+ * @author yujianjian
+ * @since 2017-12-11 下午1:56
+ */
 @TableName("blockchain_record")
 @Data
-public class BlockchainRecord extends OrderBase implements Serializable {
+public class BlockchainRecord implements Serializable {
 
     private static final long serialVersionUID = 1432748946229084964L;
 
+    private Integer id;
 
     /**
      * 交易所在块号
      */
     private Long blockNum;
 
+    /**
+     * 交易id
+     */
+    private String trxId;
 
     private String contractId;
 
@@ -33,17 +41,7 @@ public class BlockchainRecord extends OrderBase implements Serializable {
      */
     private Date trxTime;
 
-    private Integer id;
-    /**
-     * 交易id
-     */
-    private String trxId;
-    /**
-     * 订单状态,0-进行中,1-交易成功,2-交易取消,3-交易失效
-     */
-    private Integer status;
     private Date createTime;
+
     private Date updateTime;
-
-
 }
